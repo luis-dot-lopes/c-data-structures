4198afa18b0a5d391cb1ac37e460c7bb7da80677
luis-dot-lopes <luisantonio20054@gmail.com>
2024-02-20 21:59:13 -0300

    Fixed binary search algorithm

--- binary_search.c
+++ binary_search.c
@@ -10,15 +10,17 @@ returned.
 size_t
 binary_search(int elem, int* arr, size_t arr_len)
 {
-  size_t index = arr_len / 2;
-  while (index > 0 && index < arr_len &&
-         !(arr[index - 1] < elem && elem <= arr[index])) {
-    if (elem <= arr[index])
-      index /= 2;
-    if (elem > arr[index])
-      index = (arr_len + index + 1) / 2;
+  size_t upper = arr_len - 1;
+  size_t lower = 0;
+  size_t middle = (upper + lower) / 2;
+  while (middle > 0 && middle < arr_len && lower <= upper) {
+    if (elem <= arr[middle])
+      upper = middle - 1;
+    if (elem > arr[middle])
+      lower = middle + 1;
+    middle = (upper + lower) / 2;
   }
-  return index;
+  return lower;
 }
 
 void
@@ -35,10 +37,10 @@ int
 main(int argc, char const* argv[])
 {
 
-  int arr[] = { 1, 1, 2, 2, 2, 3, 6 };
+  int arr[] = { 1, 1, 2, 2, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
   size_t arr_len = sizeof(arr) / sizeof(int);
 
-  size_t index = binary_search(6, arr, arr_len);
+  size_t index = binary_search(10, arr, arr_len);
   print_arr(arr, arr_len);
   printf("%d\n", index);
 
